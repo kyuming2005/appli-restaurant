66bfa6316079052b4a20c16cceb1c22ad21d8adb
Morgan <151154716+kyuming2005@users.noreply.github.com>
2024-04-12 13:35:41 +0200

    Add files via upload

---
+++ projet restaurant a rendre/restaurantmorgan/JavaApplication2/src/appliresto/commande.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appliresto;
+
+/**
+ *
+ * @author m.denis
+ */
+public class commande {
+     private plat plat_principal;
+    private plat entrer;
+    private plat dessert;
+    private plat boisson;
+}
