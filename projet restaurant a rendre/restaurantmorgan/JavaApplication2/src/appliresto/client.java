2abffc07589ab20d5d2bc16c131b3f808216471e
Morgan <151154716+kyuming2005@users.noreply.github.com>
2024-05-02 08:35:47 +0200

    Add files via upload

---
+++ projet restaurant a rendre/restaurantmorgan/JavaApplication2/src/appliresto/client.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appliresto;
+
+
+/**
+ *
+ * @author m.denis
+ */
+public class client {
+    private client prenoms;
+}
---
+++ projet restaurant a rendre/restaurantmorgan/JavaApplication2/src/appliresto/menu.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appliresto;
+
+
+/**
+ *
+ * @author m.denis
+ */
+public class menu {
+    private menu plat_principal;
+    private menu entrer;
+    private menu dessert;
+    private menu boisson;
+  
+}
